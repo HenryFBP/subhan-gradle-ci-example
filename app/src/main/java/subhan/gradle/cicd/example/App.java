49ba6d90febc96d96ddc98b8dff73f6e29b1fd04
henryfbp <HenryFBP@gmail.com>
2022-08-16 08:34:31 -0400

    add sample test

--- app/src/main/java/subhan/gradle/cicd/example/App.java
+++ app/src/main/java/subhan/gradle/cicd/example/App.java
@@ -3,9 +3,28 @@
  */
 package subhan.gradle.cicd.example;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
+    }
+
+
+    public ArrayList<Integer> getData() {
+        Random r = new Random();
+
+        ArrayList<Integer> data = new ArrayList<>();
+
+        int i = 20;
+        while (i > 0) {
+
+            data.add(r.nextInt(0, 10));
+
+            i--;
+        }
+        return data;
     }
 
     public static void main(String[] args) {
